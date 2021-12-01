48bb9e22b8866d561e2257c0ccebdb855b2875d5
Ryan <90624129+rheien@users.noreply.github.com>
2021-12-01 16:50:02 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,8 +17,7 @@ then
 elif [ "$GITHUB_EVENT_NAME" = "pull_request" ]
 then 
   git --git-dir="$GITHUB_WORKSPACE/.git" log --left-right --cherry-pick --pretty=format:"%H" remotes/origin/$GITHUB_BASE_REF...remotes/origin/$GITHUB_HEAD_REF > commit_list.txt
-  echo gitleaks --pretty --path=$GITHUB_WORKSPACE --verbose --redact --commits-file=commit_list.txt $CONFIG
-  gitleaks --pretty --repo-path=$GITHUB_WORKSPACE --verbose --redact --commits-file=commit_list.txt $CONFIG
+  echo gitleaks --path=$GITHUB_WORKSPACE --verbose --redact --commits-file=commit_list.txt $CONFIG
   CAPTURE_OUTPUT=$(gitleaks --path=$GITHUB_WORKSPACE --verbose --redact --commits-file=commit_list.txt $CONFIG)
 fi
 
@@ -28,8 +27,8 @@ then
   echo "$GITLEAKS_RESULT"
   #echo "::set-output name=exitcode::$GITLEAKS_RESULT"
   echo "----------------------------------"
-  echo "$CAPTURE_OUTPUT"
-  #echo "::set-output name=result::$CAPTURE_OUTPUT"
+  #echo "$CAPTURE_OUTPUT"
+  echo "::set-output name=result::$CAPTURE_OUTPUT"
   echo "----------------------------------"
   exit 1
 else
