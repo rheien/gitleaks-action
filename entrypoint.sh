32644c8b94cb1d60892f4350a2af2e375ab023d8
Ryan <90624129+rheien@users.noreply.github.com>
2021-12-02 09:43:21 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -25,10 +25,10 @@ if [ $? -eq 1 ]
 then
   GITLEAKS_RESULT=$(echo -e "\e[31m🛑 STOP! Gitleaks encountered leaks")
   echo "$GITLEAKS_RESULT"
-  #echo "::set-output name=exitcode::$GITLEAKS_RESULT"
+  echo "::set-output name=exitcode::$GITLEAKS_RESULT"
   echo "----------------------------------"
   #echo "$CAPTURE_OUTPUT"
-  echo "::set-output name=result::$CAPTURE_OUTPUT"
+  #echo "::set-output name=result::$CAPTURE_OUTPUT"
   echo "----------------------------------"
   exit 1
 else
